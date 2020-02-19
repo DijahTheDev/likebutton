69da80fbbfa5ef902f67c0d664351344acdb4d53
khadijah faison <khadijahfaison@gmail.com>
2020-02-18 21:29:34 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import "./App.css"
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [name, setName] = useState("Likes")
+
+  function updateCount(e) {
+    e.preventDefault()
+    setCount(count + 1)
+
+    if (count === 0) {
+      setName("like")
+    } else {
+      setName("likes")
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={updateCount}>
+        {count} {name}
+      </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
